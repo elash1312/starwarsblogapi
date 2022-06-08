7def8c2121dd640d07ac4d82aafc3b8a5337a9fa
elash1312 <lasherin1312@gmail.com>
2022-06-08 19:57:47 +0000

    models added, still need debugging

--- src/models.py
+++ src/models.py
@@ -1,4 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
+from flask import Flask
+from flask_migrate import Migrate
 
 db = SQLAlchemy()
 
@@ -7,13 +9,67 @@ class User(db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    favorites = db.relationship('Favorites')
 
     def __repr__(self):
-        return '<User %r>' % self.username
-
+        return f'<User {self.email}>'
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
             # do not serialize the password, its a security breach
+        }
+class Favorites(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    user = db.relationship('User')
+
+    def __repr__(self):
+        return f"<Favorites {self.id}>"
+    
+    def serialize(self):
+        return {
+            'name': self.name
+        }
+
+class Characters(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    height = db.Column(db.String)
+    hair_color = db.Column(db.String)
+    eye_color = db.Column(db.String)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    user = db.relationship('User')
+
+    def __repr__(self):
+        return f"<Characters {self.id}>"
+    
+    def serialize(self):
+        return {
+        "name": self.name,
+        "height": self.height,
+        "hair color": self.hair_color,
+        "eye color": self.eye_color
+        }
+
+class Planets(db.Model):
+    __tablename__ = 'Planets'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80))
+    rotation_period = db.Column(db.String(80))
+    orbital_period = db.Column(db.String(80))
+    terrain = db.Column(db.String(80))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    user = db.relationship('User')
+
+    def __repr__(self):
+        return f"<Planets {self.id}>"
+
+    def serialize(self):
+        return {
+            "name": self.planet_name,
+            "rotation period": self.rotation_period,
+            "orbital period": self.orbital_period,
+            "terrain": self.terrain
         }
